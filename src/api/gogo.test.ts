eed969692924639d345100893a6250bb9c9dff01
Ricardo <ricarditomontserrat@gmail.com>
2025-12-29 09:12:16 +0100

    some more tests done
    M  src/api/gogo.test.ts
    M  src/api/gogo.ts

--- src/api/gogo.test.ts
+++ src/api/gogo.test.ts
@@ -1,45 +1,42 @@
-
 import { test, expect, describe } from 'bun:test';
-import { GoGo, Piece } from './gogo';
-
-// https://www.mastersofgames.com/rules/go-rules.html
-// https://www.allaboutgo.com/play-go.html
+import { GoGo, GoGoError, Piece } from './gogo';
 
 describe('Initializing Game', () => {
     const game = new GoGo();
 
-    test('should have an x by x board with all cells empty', () => {
-        expect(game.board.length).toBe(GoGo.ROWS);
-        expect(game.board[0]!.length).toBe(GoGo.COLS);
+    test('should have an x by x board with all empty cells', () => {
+        expect(game.board.length).toBe(GoGo.SIZE);
+        expect(game.board[0]!.length).toBe(GoGo.SIZE);
         game.board.forEach((row) => row.forEach((cell) => expect(cell).toBe(Piece.EMPTY)));
     });
 
-    test.todo('should be black turn and should make a move', () => {
+    test('should be black turn and should make a move', () => {
         expect(game.turn).toBe(Piece.BLACK);
-        game.makeMove(0, 0);
+        const position = game.makeMove(0, 0);
+        expect(position).toEqual({ x: 0, y: 0 });
         expect(game.board[0]![0]).toBe(Piece.BLACK);
     });
 
-    test.todo('should be white turn and make another move and fail', () => {
+    test('should be white turn and make another move and fail', () => {
         expect(game.turn).toBe(Piece.WHITE);
         expect(() => game.makeMove(0, 0)).toThrow(GoGoError.ALREADY_TAKEN);
         expect(game.turn).toBe(Piece.WHITE);
     });
 
-    test.todo('it should fail if illegal move', () => {
+    test('it should fail if illegal move', () => {
         expect(game.turn).toBe(Piece.WHITE);
         expect(() => game.makeMove(-1, 0)).toThrow(GoGoError.OUT_OF_BOUNDS);
         expect(() => game.makeMove(0, -1)).toThrow(GoGoError.OUT_OF_BOUNDS);
-        expect(() => game.makeMove(GoGo.ROWS, 0)).toThrow(GoGoError.OUT_OF_BOUNDS);
-        expect(() => game.makeMove(0, GoGo.COLS)).toThrow(GoGoError.OUT_OF_BOUNDS);
+        expect(() => game.makeMove(GoGo.SIZE, 0)).toThrow(GoGoError.OUT_OF_BOUNDS);
+        expect(() => game.makeMove(0, GoGo.SIZE)).toThrow(GoGoError.OUT_OF_BOUNDS);
         expect(game.turn).toBe(Piece.WHITE);
     })
 
-    test.todo('white should capture black', () => {
+    test('white should capture black', () => {
         expect(game.count(Piece.WHITE)).toBe(0);
         expect(game.count(Piece.BLACK)).toBe(1);
-        expect(game.prisoners[Piece.BLACK]).toBe(0);
-        expect(game.prisoners[Piece.WHITE]).toBe(0);
+        expect(game.captures[Piece.BLACK]).toBe(0);
+        expect(game.captures[Piece.WHITE]).toBe(0);
 
         game.makeMove(0, 1); // white below
         game.makeMove(1, 0); // black right
@@ -47,8 +44,10 @@ describe('Initializing Game', () => {
         game.makeMove(0, 2); // black below
         game.makeMove(2, 0); // white right
 
-        expect(game.prisoners[Piece.BLACK]).toBe(2);
-        expect(game.prisoners[Piece.WHITE]).toBe(0);
+        game.printBoard();
+
+        expect(game.captures[Piece.BLACK]).toBe(0);
+        expect(game.captures[Piece.WHITE]).toBe(2);
         expect(game.count(Piece.BLACK)).toBe(0);
         expect(game.count(Piece.WHITE)).toBe(3);
     });
@@ -69,5 +68,39 @@ describe('Initializing Game', () => {
         ];
 
         expect(game.board).toEqual(board);
+    });
+
+    test.todo('white should be winning', () => {
+        for (let i = 0; i < GoGo.SIZE; i++) {
+            for (let j = 0; j < GoGo.SIZE; j++) {
+                try { game.makeMove(i, j); } catch (e) { }
+            }
+        }
+        expect(game.captures[Piece.BLACK]).toBe(0);
+        expect(game.captures[Piece.WHITE]).toBe(42);
+    });
+
+    test.todo('should ask to end, reject, have no winner, then accept and have winner', () => {
+        game.askForEnd();
+        game.rejectEnd();
+        expect(() => game.acceptEnd()).toThrow(GoGoError.NOT_ENDED);
+        expect(game.winner).toBe(undefined);
+
+        game.askForEnd();
+        game.acceptEnd();
+        expect(game.winner).toBe(Piece.WHITE);
+    });
+
+    test.todo('should error if game is already ended', () => {
+        expect(() => game.askForEnd()).toThrow(GoGoError.ALREADY_ENDED);
+        expect(() => game.makeMove(0, 0)).toThrow(GoGoError.ALREADY_ENDED);
+    });
+
+    test.todo('should reset the game', () => {
+        game.restart();
+        expect(game.turn).toBe(Piece.BLACK);
+        expect(game.board).toEqual(Array.from({ length: GoGo.SIZE }, () => new Array(GoGo.SIZE).fill(Piece.EMPTY)));
+        expect(game.captures[Piece.BLACK]).toBe(0);
+        expect(game.captures[Piece.WHITE]).toBe(0);
     });
 });
--- src/api/gogo.ts
+++ src/api/gogo.ts
@@ -4,13 +4,70 @@ export enum Piece {
     WHITE = 2,
 }
 
+export enum GoGoError {
+    NOT_ENDED = 'No player has requested to end the game',
+    ALREADY_ENDED = 'Game has already ended',
+    OUT_OF_BOUNDS = 'Illegal move, out of bounds',
+    ALREADY_TAKEN = 'Illegal move, already taken',
+}
+
 export class GoGo {
-    static readonly ROWS = 9;
-    static readonly COLS = 9;
+    static readonly SIZE = 9;
 
     board: number[][];
+    turn: Piece = Piece.BLACK;
+    captures = { [Piece.BLACK]: 0, [Piece.WHITE]: 0 };
 
     constructor() {
-        this.board = Array.from({ length: GoGo.ROWS }, () => new Array(GoGo.COLS).fill(Piece.EMPTY));
+        this.board = Array.from({ length: GoGo.SIZE }, () => new Array(GoGo.SIZE).fill(Piece.EMPTY));
+    }
+
+    isInBounds(x: number, y: number): boolean {
+        return x >= 0 && x < GoGo.SIZE && y >= 0 && y < GoGo.SIZE;
+    }
+
+    public makeMove(x: number, y: number): { x: number, y: number } {
+        if (!this.isInBounds(x, y)) throw new Error(GoGoError.OUT_OF_BOUNDS);
+        if (this.board[y]![x] !== Piece.EMPTY) throw new Error(GoGoError.ALREADY_TAKEN);
+
+        this.board[y]![x] = this.turn;
+        this.turn = this.turn === Piece.BLACK ? Piece.WHITE : Piece.BLACK;
+
+        let i, j;
+        for (let [di, dj] of [[1, 0], [0, 1], [-1, 0], [0, -1], [0, 0]]) {
+            i = y + di!;
+            j = x + dj!;
+            const piece = this.board[i]?.[j];
+            const other = piece === Piece.BLACK ? Piece.WHITE : Piece.BLACK;
+            if (!piece || piece === Piece.EMPTY) continue;
+
+            let liberties = 4;
+            let inBattle = false;
+            for (let [dx, dy] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+                const bx = i + dx!;
+                const by = j + dy!;
+                if (!this.isInBounds(bx, by) || this.board[by]![bx] !== Piece.EMPTY) {
+                    liberties--;
+                    inBattle = by === y && bx === x || inBattle;
+                }
+            }
+
+            if (liberties === 0 && inBattle) {
+                this.captures[other]++;
+                this.board[i]![j] = Piece.EMPTY;
+            }
+        }
+
+        return { x, y };
+    }
+
+    public count(piece: Piece): number {
+        return this.board.reduce((count, row) => count + row.filter((cell) => cell === piece).length, 0);
+    }
+
+    public printBoard() {
+        console.log();
+        this.board.forEach((row) => console.log(row.join(' ')));
+        console.log();
     }
 }
